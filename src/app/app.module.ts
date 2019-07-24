e57c9a9ce54cbaf15435f5d8a3cc9d64c564a9c5
Castillo Camizan <castilloesteban2009@gmail.com>
2019-07-24 17:46:47 -0500

    Es Angular Material Design

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+//Animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Material
+import { MaterialModule } from './material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
